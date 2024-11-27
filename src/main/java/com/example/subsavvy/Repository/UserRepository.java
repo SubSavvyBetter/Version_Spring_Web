0ee7404d4f9ba7eaeaa727ca7f2040063d937b57
KajikaSan <charlotte.bigare@gmail.com>
2024-11-27 14:48:23 +0100

    fix db issues

--- src/main/java/com/example/subsavvy/Repository/UserRepository.java
+++ src/main/java/com/example/subsavvy/Repository/UserRepository.java
@@ -6,8 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findUserById(String id);
-    User saveUser(User usertoadd);
-    void deleteUser(User usertodel);
-    List<User> getAllUsers();
 }
