a877a4a80b60e4c3a42b825472c243c9310cc500
KajikaSan <charlotte.bigare@gmail.com>
2024-12-06 17:18:31 +0100

    Add routes of Subscription

--- src/main/java/com/example/subsavvy/Repository/UserRepository.java
+++ src/main/java/com/example/subsavvy/Repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.subsavvy.Data.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
---
+++ src/main/java/com/example/subsavvy/Service/SubscriptionService.java
@@ -0,0 +1,43 @@
+package com.example.subsavvy.Service;
+
+import com.example.subsavvy.Data.Subscription;
+import com.example.subsavvy.Repository.SubscriptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class SubscriptionService {
+
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
+
+     public List<Subscription> getAllSubscriptions() {
+        return subscriptionRepository.findAll();
+    }
+
+    public Optional<Subscription> getSubscriptionById(UUID id) {
+        return subscriptionRepository.findById(id);
+    }
+
+     public Subscription addSubscription(Subscription subscription) {
+        return subscriptionRepository.save(subscription);
+    }
+
+    public Subscription updateSubscription(UUID id, Subscription subscription) {
+        // Vérifie si l'abonnement existe
+        if (subscriptionRepository.existsById(id)) {
+            subscription.setId(id);
+            return subscriptionRepository.save(subscription);
+        } else {
+            throw new RuntimeException("Sub not find");
+        }
+    }
+
+    public void deleteSubscription(UUID id) {
+        subscriptionRepository.deleteById(id);
+    }
+}
