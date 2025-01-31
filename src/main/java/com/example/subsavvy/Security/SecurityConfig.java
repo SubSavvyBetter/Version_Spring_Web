7f9858405406292ec36e407af515d103deebda8d
T1masv <timotei.gerante@gmail.com>
2025-01-31 11:23:16 +0100

    Restrict allowed CORS origins to the production domain.
    Updated the CORS configuration to allow requests only from "https://subsavvy.xyz" instead of all origins ("*"). This change enhances security by limiting access to trusted sources.

--- src/main/java/com/example/subsavvy/Security/SecurityConfig.java
+++ src/main/java/com/example/subsavvy/Security/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("https://subsavvy.xyz"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
