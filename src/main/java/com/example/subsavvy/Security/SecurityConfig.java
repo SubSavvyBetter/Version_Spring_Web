74dfb65c0f3aedfccd916cf52ec86a1bd8fce668
T1masv <timotei.gerante@gmail.com>
2025-01-31 11:33:16 +0100

    Enable CORS configuration in SecurityConfig
    Added a CORS configuration source to the security filter chain. This ensures that the application handles cross-origin resource sharing properly, improving compatibility with client-side requests from different origins.

--- src/main/java/com/example/subsavvy/Security/SecurityConfig.java
+++ src/main/java/com/example/subsavvy/Security/SecurityConfig.java
@@ -24,10 +24,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+        http
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                    .anyRequest().permitAll()
+            );
         return http.build();
     }
     @Bean
